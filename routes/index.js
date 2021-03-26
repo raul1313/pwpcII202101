37d6152bbd4b51ef0bad87b82cef50e2f4bead84
raul1313 <yugi6703@gmail.com>
2021-03-26 11:54:59 -0600

    proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Gonzaleez Raul', NameApp: 'WepApp' });
+});
+
+router.get('/greeting', function(req, res, next ){
+res.send('Hola Campeon')
+})
+
+
+router.get('/hola', function(req, res, next ){
+ res.status(200).json({message: 'Gonzalez Mendoza Raul Jonoel'})
+  })
+
+
+
+
+module.exports = router;
