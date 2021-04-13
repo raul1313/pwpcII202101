5d60ee265e07a25940c44b1d81367c635390831a
raul1313 <yugi6703@gmail.com>
2021-04-12 21:20:34 -0500

    agregando compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Gonzaleez Raul', NameApp: 'WepApp' });
+  res.render('index', { title: 'Express', author: 'Gonzaleez Raul', NameApp: 'WepApp', Company: 'Awsome Sofware' });
 });
 
 router.get('/greeting', function(req, res, next ){
